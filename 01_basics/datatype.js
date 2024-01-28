e4cb76922959a28f76a1362c6687e40809ed673d
MahaveerGiwariya <152847488+MahaveerGiwariya@users.noreply.github.com>
2024-01-28 14:25:28 +0000

    add on

---
+++ 01_basics/datatype.js
@@ -0,0 +1,22 @@
+ "use strict"; // treat all js code as newer version
+
+// alert ("hello") 
+ //alert(3 + 3) // we are using nodejs ,not browser
+console.log( 3 + 3); // code readability should be high 
+console.log( "mahaveer");
+// documantation mdn and ecma (tc39)
+
+let name = "mahaveer"
+let age = 19
+let isLoggedIn = false
+
+// Number => 2 to power 53 (memory range)
+// bigint( big namer ex- facebook ,trading,stockmarket)
+//Boolean => true /false
+//null => standalone value 
+//undefined => abhi value assign nhi hui h
+// symbol(react more use and uniq)
+// object
+ console.log(typeof null ); // objet datatype 
+ console.log(typeof undefined); // undefined datatype
+

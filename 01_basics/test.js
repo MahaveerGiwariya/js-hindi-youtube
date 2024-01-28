d9e33f1140adee6d5aaa2536bbd69a29f8e856ab
MahaveerGiwariya <152847488+MahaveerGiwariya@users.noreply.github.com>
2024-01-28 14:01:33 +0000

    few changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("mahaveer")
+const accountId = 1145
+let accountEmail = "hitash@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+console.log(accountId);
+// accountId = 2   //not alowed because const dont change 
+accountPassword = "22222"
+accountEmail = "mahaveer@gamil.com"
+accountCity = "ajmer"
+
+/* prefer to not use var
+because  in this isuue of blockscope and funcational */
+console.table([ ,accountEmail, accountPassword, accountCity]);
